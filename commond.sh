5d26fc9b94d7b77bc5107a9409674529ae569b79
chenqg <1304085583@qq.com>
2023-01-18 17:03:06 +0800

    no message

---
+++ commond.sh
@@ -0,0 +1,6 @@
+# 启动命令示例
+# 启动
+docker-compose up -d --scale worker=2
+# 停止
+docker-compose down
+
--- project_setting.py
+++
@@ -1,3 +0,0 @@
-UA_API_HOST = "https://api.xxx.cn"
-COURSE_API_HOST = "https://api2.xxx.cn"
-WWW_HOWS = "https://api3.xxx.cn"
