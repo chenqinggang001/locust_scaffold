360e7340770083acef73be9d7e3e564255c8f562
chenqg <1304085583@qq.com>
2023-01-29 16:45:35 +0800

    no message

--- common/redis_store.py
+++ common/redis_store.py
@@ -1,6 +1,9 @@
+import os
+
 import redis
 
-pool = redis.ConnectionPool(host='redis', port=6379, password='cqg123456!', db=0, decode_responses=True)
+# 这个地方的host地址依赖docker中运行时的环境变量，如果不是docker运行，需要自己修改
+pool = redis.ConnectionPool(host=os.environ.get('REDIS_HOST'), port=6379, password='rdspwd123456!', db=0, decode_responses=True)
 redis_store = redis.Redis(connection_pool=pool, decode_responses=True)
 
 
--- commond.sh
+++ commond.sh
@@ -1,3 +1,5 @@
+# 启动前先授予/data/locust目录权限
+chmod -R 777 /data/locust
 # 启动命令示例
 # 启动
 docker-compose up -d --scale worker=2
@@ -5,3 +7,5 @@ docker-compose up -d --scale worker=2
 docker-compose down
 # 分布式情况下从机上单独启动worker,cpu有多少个核心就启动多少个worker
 docker-compose up -d --scale worker=3 worker
+# grafana容器部署命令
+docker run -d -p 3000:3000 --name grafana grafana/grafana
